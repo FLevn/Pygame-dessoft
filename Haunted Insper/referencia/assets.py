c939a1b065583cbd2f86412a162a1c3c6df31431
Guilherme Sampaio <guisamps@gmail.com>
2023-05-22 11:47:07 -0300

    changes

--- Haunted Insper/referencia/assets.py
+++ Haunted Insper/referencia/assets.py
@@ -12,8 +12,8 @@ player_IMG = 'player_img'
 def load_assets():
     assets = {}
     assets[BACKGROUND] = pygame.image.load(os.path.join(IMG_DIR, '')).convert()
-    assets[ghost_IMG] = pygame.image.load(os.path.join(IMG_DIR, '')).convert_alpha()
-    assets[ghost_IMG] = pygame.transform.scale(assets[''], (ghost_WIDTH, ghost_HEIGHT))
-    assets[player_IMG] = pygame.image.load(os.path.join(IMG_DIR, '')).convert_alpha()
-    assets[player_IMG] = pygame.transform.scale(assets[''], (player_WIDTH, player_HEIGHT))
+    assets[ghost_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'ghost.png')).convert_alpha()
+    assets[ghost_IMG] = pygame.transform.scale(assets['ghost.png'], (ghost_WIDTH, ghost_HEIGHT))
+    assets[player_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'player.png')).convert_alpha()
+    assets[player_IMG] = pygame.transform.scale(assets['player.png'], (player_WIDTH, player_HEIGHT))
     return assets
