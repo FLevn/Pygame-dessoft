8a5c46d0ef062d6a2927bfcf305ff0141b0b7554
Felipe <felipe.leventhal@gmail.com>
2023-05-28 20:57:26 -0300

    anotações

--- Jogo/player.py
+++ Jogo/player.py
@@ -45,17 +45,17 @@ class Player(pygame.sprite.Sprite):
 		if direction == 'horizontal':
 			for sprite in self.obstacle_sprites:
 				if sprite.hitbox.colliderect(self.hitbox):
-					if self.direction.x > 0: # moving right
+					if self.direction.x > 0: # direita
 						self.hitbox.right = sprite.hitbox.left
-					if self.direction.x < 0: # moving left
+					if self.direction.x < 0: # esquerda
 						self.hitbox.left = sprite.hitbox.right
 
 		if direction == 'vertical':
 			for sprite in self.obstacle_sprites:
 				if sprite.hitbox.colliderect(self.hitbox):
-					if self.direction.y > 0: # moving down
+					if self.direction.y > 0: # baixo
 						self.hitbox.bottom = sprite.hitbox.top
-					if self.direction.y < 0: # moving up
+					if self.direction.y < 0: # cima
 						self.hitbox.top = sprite.hitbox.bottom
 
 	def update(self):
