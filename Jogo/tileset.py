5d1a4719fa92ce1b7747e7b90505038c53eda509
Felipe <felipe.leventhal@gmail.com>
2023-05-29 09:07:12 -0300

    asssets

--- Jogo/tileset.py
+++ Jogo/tileset.py
@@ -4,7 +4,7 @@ from config import *
 class Tile(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
-		self.image = pygame.image.load('stonebricks-1.png.png').convert_alpha()
+		self.image = pygame.image.load('cobble brick wall.png').convert_alpha()
 		self.image = pygame.transform.scale(self.image, (TILESIZE, TILESIZE))
 		self.rect = self.image.get_rect(topleft = pos)
 		self.hitbox = self.rect.inflate(0,-10)
