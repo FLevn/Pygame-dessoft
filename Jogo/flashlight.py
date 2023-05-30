fc7efb6facf599d7eea50e1eec2c00708efeeb0c
Felipe <felipe.leventhal@gmail.com>
2023-05-30 11:02:29 -0300

    flashlight

--- Jogo/flashlight.py
+++ Jogo/flashlight.py
@@ -9,7 +9,7 @@ class Flashlight(pygame.sprite.Sprite):
 		# gráfico
 		full_path = f'assets/img/flashlight/{direction}.png'
 		self.image = pygame.image.load(full_path).convert_alpha()
-		self.image = pygame.transform.scale(self.image, (32, 32))
+		self.image = pygame.transform.scale(self.image, (64, 64))
 		
 		# posicionamento
 		if direction == 'right':
