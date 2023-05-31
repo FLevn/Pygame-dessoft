122445ca9f06078c1516f7623ef2afe14be2e765
GabrielVidigal1 <gabrielcv1@al.insper.edu.br>
2023-05-30 23:09:50 -0300

    Temporizador scuffed

--- Jogo/game_screen.py
+++ Jogo/game_screen.py
@@ -7,7 +7,7 @@ pygame.mixer.init()
 pygame.mixer.music.load('assets/Sons/trilha.mp3')
 pygame.mixer.music.set_volume(1)
 pygame.mixer.music.play(loops=-1)
-
+font=pygame.font.Font(None,36)
 
 class Game:
 	def __init__(self):
@@ -23,7 +23,16 @@ class Game:
 		self.timer = Timer(self.screen)
 	
 	def run(self):
+		countframes=0
+		tempo=0
 		while True:
+			countframes+=1
+			if countframes>=FPS:
+				tempo+=1
+				countframes=0
+			showtime=font.render('tempo:  '+str(tempo),True,WHITE)
+			self.screen.blit(showtime,200,10)
+
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					pygame.quit()
